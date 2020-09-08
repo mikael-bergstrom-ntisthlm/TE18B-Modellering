f9909c6a82d4688045da36e02bb389a4db571e87
Mikael <mikael.bergstrom@ga.ntig.se>
2020-09-08 10:31:09 +0200

    Inital commit

---
+++ Modellering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modellering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
